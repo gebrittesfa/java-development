48ca4e29e411f035ebc08e14d0e629b1bc318f70
gebrittesfa <gebrey08@gmail.com>
2024-05-25 13:22:23 -0400

    fixed

---
+++ Workbook-6/TurtlePaint/src/main/java/com/pluralsight/Circle.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class Circle extends Shape{
+    private int radius;
+
+    Circle(int x, int y, String color ,int border, int width, int height,int radius){
+        super(x,y,color ,border, width,height);
+        this.radius=radius;
+    }
+    // @Override
+    public void paint(){
+        System.out.println("Painting a circle at (" + super.x + ", " + super.y + ") " +
+                "with border color " + super.border + " and radius " + this.radius);
+
+    }
+}
---
+++ Workbook-6/TurtlePaint/src/main/java/com/pluralsight/Square.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+public class Square extends Shape{
+
+    Square(int x, int y, String color ,int border, int width, int height){
+        super(x,y,color ,border, width,height);
+
+    }
+    @Override
+    public void paint() {
+        System.out.println("Painting a square at (" + x + ", " + y + ") with border color "+ color+
+                + border + " and size " + width);
+    }
+}
